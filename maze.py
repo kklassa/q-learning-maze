de3ac0e1e658befdac562ec8b3640e5928809563
Kacper Klassa <kacperk143@gmail.com>
2022-01-15 19:32:27 +0100

    refactor maze environment creation and interaction

--- maze.py
+++ maze.py
@@ -1,31 +1,144 @@
 import numpy as np
+import pygame
+import json
 
 
-def prepare_maze(filepath):
-    maze = []
-    rows = 0
-    with open(filepath, 'r') as fh:
-        for line in fh:
-            row = [character for character in line if character != '\n']
-            if row:
-                rows +=1
-            maze.append(row)
-    columns = len(maze[0])
 
-    maze_board = np.zeros((rows, columns))
+CORRIDOR_CODE = 0
+WALL_CODE = -1
+POSITION_CODE = -5
+END_CODE = -10
+VISITED_CODE = 1
 
-    for row_idx in range(rows):
-        for column_idx in range(columns):
-            char = maze[row_idx][column_idx]
-            if char == '.':
-                maze_board[row_idx][column_idx] = 0
-            elif char =='#':
-                maze_board[row_idx][column_idx] = 1
-            elif char == 'S':
-                maze_board[row_idx][column_idx] = 5
-                start = np.array([row_idx, column_idx])
-            elif char == 'F':
-                maze_board[row_idx][column_idx] = 10
-                end = np.array([row_idx, column_idx])
 
-    return maze_board, start, end
+class Maze():
+    def __init__(self, layout_filepath):
+        self.load_maze(layout_filepath)
+        self.agent_position = self.start_pos
+
+
+    def get_layout(self):
+        return self.layout
+
+
+    def get_start_position(self):
+        return self.start_pos
+
+
+    def get_end_position(self):
+        return self.end_pos
+
+
+    def get_agent_position(self):
+        return self.agent_position
+
+
+    def set_agent_position(self, new_position):
+        row, col = self.agent_position
+        self.layout[row][col] = VISITED_CODE
+
+        self.agent_position = new_position
+        new_row, new_col = new_position
+        self.layout[new_row][new_col] = POSITION_CODE
+
+        return (self.agent_position==self.end_pos).all()
+
+
+    def load_maze(self, filepath):
+        '''
+        Loads a maze from a text file containing
+        ASCII characters - . for floor, # for wall
+        S for start position and F for end position
+        '''
+        maze = []
+        rows = 0
+        with open(filepath, 'r') as fh:
+            for line in fh:
+                row = [character for character in line if character != '\n']
+                if row:
+                    rows +=1
+                maze.append(row)
+        columns = len(maze[0])
+        maze_board = np.zeros((rows, columns))
+        for row_idx in range(rows):
+            for column_idx in range(columns):
+                char = maze[row_idx][column_idx]
+                if char == '.':
+                    maze_board[row_idx][column_idx] = CORRIDOR_CODE
+                elif char =='#':
+                    maze_board[row_idx][column_idx] = WALL_CODE
+                elif char == 'S':
+                    maze_board[row_idx][column_idx] = POSITION_CODE
+                    self.start_pos = np.array([row_idx, column_idx])
+                elif char == 'F':
+                    maze_board[row_idx][column_idx] = END_CODE
+                    self.end_pos = np.array([row_idx, column_idx])
+        self.layout = maze_board
+
+
+    def get_actions(self):
+        '''
+        Agent can move up, down, left and right.
+        It cannot pass through walls or leave the grid
+        '''
+        max_row, max_col = self.layout.shape
+        row, col = self.agent_position
+        available_positions = []
+        for offset in [-1, 1]:
+            if row + offset >= 0 and row + offset < max_row and self.layout[row + offset][col] != WALL_CODE:
+                available_positions.append([row + offset, col])
+            if col + offset >= 0 and col + offset <max_col and self.layout[row, col + offset] != WALL_CODE:
+                available_positions.append([row, col + offset])
+
+        return np.array(available_positions)
+
+
+    def load_color_theme(self, filepath, theme_name):
+        with open(filepath) as fh:
+            themes = json.load(fh)
+        self.theme = themes[theme_name][0]
+
+
+    def prepare_window(self, square_size, caption='Maze'):
+        pygame.init()
+        self.square = square_size
+        self.default_caption = caption
+        rows, columns = self.layout.shape
+        height = rows * self.square
+        width = columns * self.square
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption(self.default_caption)
+        self.window.fill(self.theme['corridor_color'])
+
+
+    def render(self, caption=None):
+        self.window.fill(self.theme['corridor_color'])
+        for row in range(self.layout.shape[0]):
+            for col in range(self.layout.shape[1]):
+                square = self.layout[row][col]
+                if square:
+                    points = [
+                        (col * self.square, row * self.square),
+                        (col * self.square + self.square, row * self.square),
+                        (col * self.square + self.square, row * self.square + self.square),
+                        (col * self.square, row * self.square + self.square),
+                    ]
+                    if square == WALL_CODE:
+                        square_color = self.theme['wall_color']
+                    elif square == POSITION_CODE:
+                        square_color = self.theme['position_color']
+                    elif square == END_CODE:
+                        square_color = self.theme['end_color']
+                    elif square == VISITED_CODE:
+                        square_color = self.theme['visited_color']
+                    pygame.draw.polygon(self.window, square_color, points)
+        if caption:
+            pygame.display.set_caption(caption)
+
+        pygame.display.update()
+
+
+
+
+if __name__ == "__main__":
+    pass
--- simulation.py
+++ simulation.py
@@ -1,60 +1,10 @@
-from os import stat
-from turtle import pos
 import numpy as np
 import pygame
 import sys
 from time import sleep
-import json
 
-from maze import prepare_maze
+from maze import Maze
 
-
-
-def draw_maze(board, square_size, color_theme):
-    rows, columns = board.shape
-    height = rows * square_size
-    width = columns * square_size
-    screen = pygame.display.set_mode((width, height))
-    screen.fill(color_theme['floor_color'])
-
-    return screen
-
-def update_maze(screen, board, square_size, color_theme):
-    screen.fill(color_theme['floor_color'])
-    for row in range(board.shape[0]):
-        for col in range(board.shape[1]):
-            square = board[row][col]
-            if square:
-                points = [
-                    (col * square_size, row * square_size),
-                    (col * square_size + square_size, row * square_size),
-                    (col * square_size + square_size, row * square_size + square_size),
-                    (col * square_size, row * square_size + square_size),
-                ]
-                if square == 1:
-                    square_color = color_theme['wall_color']
-                elif square == 5:
-                    square_color = color_theme['position_color']
-                elif square == 10:
-                    square_color = color_theme['end_color']
-                elif square == 4:
-                    square_color = color_theme['visited_color']
-                pygame.draw.polygon(screen, square_color, points)
-
-    return screen
-
-def available_moves(board, position):
-    max_row, max_col = board.shape
-    offsets = [-1, 0, 1]
-    available_positions = []
-    for row_off in offsets:
-        for col_off in offsets:
-            row = position[0] + row_off
-            col = position[1] + col_off
-            if row >= 0 and col >= 0 and row < max_row and col < max_col and not (row == position[0] and col == position[1]):
-                if board[row][col] != 1:
-                    available_positions.append((row, col))
-    return np.array(available_positions)
 
 def euclidean_heuristic(board, moves, end):
     prob = []
@@ -67,66 +17,44 @@ def euclidean_heuristic(board, moves, end):
         if min_distance > distance:
             min_index = index
         if board[row][col] == 0:
-            prob.append(2)
-        elif board[row][col] == 4:
+            prob.append(1.5)
+        elif board[row][col] == 1:
             prob.append(1)
         else:
             prob.append(0)
-    prob[min_index] = 3
+    prob[min_index] = 1.6
     sum = np.sum(prob)
     prob = [p / sum for p in prob]
 
     return moves[np.random.choice(moves.shape[0], 1, p=prob), :].flatten()
 
-def calculate_rewards(moves, board):
-    pass
-
 
 def main():
 
+    env = Maze('resources/maze3.txt')
+    env.load_color_theme('resources/themes.json', 'prison')
+    env.prepare_window(20)
 
-
-    filepath = 'resources/maze.txt'
-    with open('resources/themes.json') as fh:
-        themes = json.load(fh)
-
-    color_theme = themes['pastel'][0]
-
-    pygame.init()
-    maze, start, end = prepare_maze(filepath)
-    square_size = 20
-    screen = draw_maze(maze, square_size, color_theme)
-    update_maze(screen, maze, square_size, color_theme)
-
-    pygame.display.set_caption('maze')
-
-    position = start
     iterations = 0
-    sim_over = False
-    while not sim_over:
+    end_found = False
+    while not end_found:
         iterations += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        row, col = position
-        maze[row][col] = 4 # mark as visited
-        av_pos = available_moves(maze, position)
-        # new_position = av_pos[np.random.randint(av_pos.shape[0], size=1), :].flatten()
-        new_position = euclidean_heuristic(maze, av_pos, end)
-        row, col = new_position
-        maze[row][col] = 5
+        actions = env.get_actions()
+        new_position = actions[np.random.randint(actions.shape[0], size=1), :].flatten()
 
-        print(f'Position: {position}\nChosen next move: {new_position}\nIteration: {iterations}')
-        position = new_position
+        result = env.set_agent_position(new_position)
 
-        update_maze(screen, maze, square_size, color_theme)
+        env.render(f'Iteration: {iterations}')
 
-        sleep(0.01)
-        pygame.display.update()
+        sleep(0.005)
 
-        if (position==end).all():
+        if result:
             print(f'Found exit in {iterations} iterations')
+            end_found = True
             sleep(3)
             sys.exit()
 
