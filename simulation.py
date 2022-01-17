aa829803acff17d015c1e15d5430370df91dea2f
Kacper Klassa <kacperk143@gmail.com>
2022-01-17 22:46:25 +0100

    report test setup

--- simulation.py
+++ simulation.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from maze import Maze
 from q_learning import Q_learning, Q_learning_visual
 from plotter import plot_q_stats
@@ -5,10 +7,14 @@ from plotter import plot_q_stats
 
 def main():
 
-    env = Maze('resources/maze3.txt')
+    env = Maze('resources/maze-16x16.txt')
 
-    episodes = 100
-    path, rewards, lenghts = Q_learning_visual(env, episodes, 1, 0.2, 0.00005)
+    episodes = 300
+    path, rewards, lenghts = Q_learning(env, episodes, 1.0, 0.6)
+
+    sample = 100
+    for i in range(int(episodes/sample)):
+        print(f'Episodes {i*sample}-{(i+1)*sample} mean reward: {np.mean(rewards[i*sample:(i+1)*sample])}')
 
     plot_q_stats(rewards, lenghts)
     env.visualize_path(path)
